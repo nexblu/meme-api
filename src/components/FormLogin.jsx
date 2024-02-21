8c3410990da663704aa73a13d51be63abe1c8f2d
nexblu <farras.pramudita@gmail.com>
2024-02-21 16:35:10 +0700

    refactoring: remove usestate error

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,38 +1,30 @@
 import { Form, Button } from 'react-bootstrap';
 import { useState } from 'react';
+import Cookies from 'js-cookie';
+import { useNavigate } from "react-router-dom";
 
 const FormLogin = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const [error, setError] = useState(true);
-
     const [usernameError, setUsernameError] = useState('');
     const [passwordError, setPasswordError] = useState('');
 
-    const onRegis = () => {
-        const url = 'http://localhost:8000/api/v1/meme-api/register';
-        const data = {
-            email: email,
-            password: password
-        };
+    const navigate = useNavigate();
 
-        fetch(url, {
-            method: 'POST',
+    const onLogin = () => {
+        fetch(`http://127.0.0.1:5000/api/v1/nexblu/login/${username}/${password}`, {
+            method: 'GET',
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+                'Accept': 'application/json'
+            }
         })
-            .then(response => {
-                return response.json();
-            })
-            .then(json => {
-                console.log('Response:', json);
-                if (json[0]['meme-api']['status_code'] === 201) {
-                    setEmail('');
-                    setPassword('');
+            .then(response => response.json())
+            .then(data => {
+                if (data['status_code'] === 200) {
+                    const token = data['token'];
+                    Cookies.set('access_token', token);
+                    navigate('/')
                 }
             })
             .catch(error => {
@@ -41,40 +33,49 @@ const FormLogin = () => {
     }
 
     const validation = () => {
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 3; i++) {
+            let isError = false;
             if (username === '') {
                 setUsernameError('This value is required.');
-                setError(true);
+                isError = true;
             } else {
                 setUsernameError('');
-                setError(false);
             }
             if (password === '') {
                 setPasswordError('This value is required.');
-                setError(true);
+                isError = true;
             } else {
                 setPasswordError('');
-                setError(false);
             }
+            return isError;
         }
     }
 
+    const clearForm = () => {
+        setUsernameError('');
+        setUsername('');
+        setPasswordError('');
+        setPassword('');
+    }
+
     const handleSubmit = () => {
-        validation()
-        console.log(error)
+        const resultValidation = validation()
+        if (resultValidation === false) {
+            onLogin()
+        }
     }
 
     return (
         <Form className='me-3 ms-3 mt-3'>
             <Form.Group className="mb-3" controlId="formGroupText">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} className={`${usernameError === 'This value is required.' ? 'border-danger': ''}`} />
-                {usernameError === 'This value is required.' ? <p className='text-danger'>{usernameError}</p>: ''}
+                <Form.Control type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} className={`${usernameError === 'This value is required.' ? 'border-danger' : ''}`} />
+                {usernameError === 'This value is required.' ? <p className='text-danger'>{usernameError}</p> : ''}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} className={`${passwordError === 'This value is required.' ? 'border-danger': ''}`}/>
-                {passwordError === 'This value is required.' ? <p className='text-danger'>{passwordError}</p>: ''}
+                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} className={`${passwordError === 'This value is required.' ? 'border-danger' : ''}`} />
+                {passwordError === 'This value is required.' ? <p className='text-danger'>{passwordError}</p> : ''}
             </Form.Group>
             <Button variant="primary" className='mb-3' onClick={handleSubmit}>Login</Button>{' '}
             <div className="d-flex flex-row mb-3">
